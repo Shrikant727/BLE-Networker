b61bcad8590378d1e14bc17200e105a447817a9a
Shrikant727 <shirkantbhadgaonkar@gmail.com>
2023-04-15 12:41:01 +0530

    fixed async calls

--- aid_connect/lib/home.dart
+++ aid_connect/lib/home.dart
@@ -1,9 +1,13 @@
+import 'dart:async';
+import 'dart:io';
 import 'dart:typed_data';
 import 'package:uuid/uuid.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_ble_peripheral/flutter_ble_peripheral.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
+import 'dart:typed_data';
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
   final String title;
@@ -13,7 +17,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   @override
-  List<String>user_data=[];
+  List user_data=[];
   bool advertising = false;
   initState(){
     super.initState();
@@ -22,11 +26,10 @@ class _MyHomePageState extends State<MyHomePage> {
   }
   Future<void> setup() async {
     String uid=await generateuuid();
-    user_data.add(uid);
-    List data=await getinfo();
-    for(int i=0;i<data.length;i++){
-      user_data.add(data[i]);
-    }
+    // user_data.add(255);
+    // (utf8.encode(uid!)).forEach((element) {user_data.add(element);});
+  await getinfo();
+    print(user_data);
     await requestPermissions();
   }
   final _textcontroller = TextEditingController();
@@ -57,13 +60,18 @@ class _MyHomePageState extends State<MyHomePage> {
             textbox(_textcontroller),
           ElevatedButton(
           onPressed: () {
+            FocusScope.of(context).requestFocus(FocusNode());
             advertise(_textcontroller.text,0,user_data);
+            _textcontroller.clear();
               },
           child: Text('Send'),
     ),
             ElevatedButton(
-                onPressed:(){
-                  blePeripheral.stop();
+                onPressed:()async{
+                  print(user_data);
+                  if(await blePeripheral.isAdvertising) {
+                    blePeripheral.stop();
+                  }
                   setState(() {
                     advertising=false;
                   });
@@ -77,15 +85,29 @@ class _MyHomePageState extends State<MyHomePage> {
 
 
   Future<void> advertise(String data,int counter,user_data) async {
+    print(user_data);
+    List<int> send=[];
+    send.add(counter);
+    send.add(255);
+    (user_data).forEach((element) {send.add(element);});
+    send.add(255);
+    (utf8.encode(data)).forEach((element) {send.add(element);});
+    GZipCodec codec = GZipCodec();
+    // List<int> compressedData = codec.encode(send);
+    // print(compressedData);
+    // send=compressedData;
     if(counter<=255){
+      int delimiter=255;
     final advertiseSettings= AdvertiseSettings(advertiseMode: AdvertiseMode.advertiseModeLowLatency,
         txPowerLevel: AdvertiseTxPower.advertiseTxPowerHigh);
-    Uint8List counterList = Uint8List.fromList([counter]);
+    Uint8List payload = Uint8List.fromList(send);
     final AdvertiseData advertiseData = AdvertiseData(
       serviceUuid: 'bf27730d-860a-4e09-889c-2d8b6a9e0fe7',
       manufacturerId: 1234,
-      manufacturerData: counterList,
+      manufacturerData: payload,
     );
+    print(payload);
+    _textcontroller.clear();
     await blePeripheral.start(advertiseData: advertiseData,advertiseSettings: advertiseSettings);
     setState(() {
       advertising=true;
@@ -146,72 +168,78 @@ Future<String> generateuuid() async {
   }
   return uuid;
 }
-Future<List<String?>> getinfo() async {
+Future<void> getinfo() async {
   SharedPreferences prefs = await SharedPreferences.getInstance();
   String? phone = prefs.getString('phone');
+  if(phone==null||phone.isEmpty){
+      List data= await getform();
+        prefs.setString('phone', data[0]);
+        prefs.setString('name', data[1]);
+        prefs.setString('extra', data[2]);
+  }
+  phone = prefs.getString('phone');
   String? name = prefs.getString('name');
   String? extra = prefs.getString('extra');
-  if(phone==null||phone.isEmpty){
-      List data=await getform();
-      prefs.setString('phone', data[0]);
-      prefs.setString('name', data[1]);
-      prefs.setString('extra', data[2]);
-  }
   debugPrint('phone: $phone, name: $name, extra: $extra');
-  return [phone,name,extra];
-}
+  user_data.add(255);
+  (utf8.encode(phone!)).forEach((element) {user_data.add(element);});
+  user_data.add(255);
+  // (utf8.encode(name!)).forEach((element) {user_data.add(element);});
+  // user_data.add(255);
+  // (utf8.encode(extra!)).forEach((element) {user_data.add(element);});
+  // user_data.add(255);
 
-//show dialog which uses createform() to get the name and extra info
-Future<List> getform()async {
-    List<String>data =[];
-  showDialog(
-    context: context,
-    builder: (BuildContext context){
-      return AlertDialog(
-        title: const Text("Please Enter your Phone, Name and any additional information you would like to share"),
-        content: createform(),
-        actions: [
-          TextButton(
-            child: const Text("Submit"),
-            onPressed: () {
-              data.add(_phonecontroller.text);
-              data.add(_namecontroller.text);
-              data.add(_extracontroller.text);
-              Navigator.of(context).pop();
-            },
-          ),
-        ],
-      );
-    });
-  return data;
 }
-Widget createform(){
-  return Form(
-    child: Column(
-      children: [
-        TextFormField(
-          controller: _phonecontroller,
-          decoration: InputDecoration(
-            border: OutlineInputBorder(),
-            labelText: 'Phone Number',
+  Future<List<String>> getform() async {
+    List<String> data = [];
+    Completer<List<String>> completer = Completer<List<String>>();
+    await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text("Please Enter your Phone, Name and any additional information you would like to share"),
+          content: SingleChildScrollView(
+            child: Column(
+              children: [
+                TextFormField(
+                  controller: _phonecontroller,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Phone Number',
+                  ),
+                ),
+                TextFormField(
+                  controller: _namecontroller,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Name',
+                  ),
+                ),
+                TextFormField(
+                  controller: _extracontroller,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Extra Additional Information'),
+                ),
+              ],
+            ),
           ),
-        ),
-        TextFormField(
-          controller: _namecontroller,
-          decoration: InputDecoration(
-            border: OutlineInputBorder(),
-            labelText: 'Name',
-          ),
-        ),
-        TextFormField(
-          controller: _extracontroller,
-          decoration: InputDecoration(
-            border: OutlineInputBorder(),
-            labelText: 'Extra Additional Information'
-          ),
-        ),
-      ],
-    ),
-  );
-}
+          actions: [
+            TextButton(
+              child: const Text("Submit"),
+              onPressed: () {
+                data.add(_phonecontroller.text);
+                data.add(_namecontroller.text);
+                data.add(_extracontroller.text);
+                Navigator.of(context).pop();
+                completer.complete(data);
+              },
+            ),
+          ],
+        );
+      },
+    );
+    return completer.future;
+  }
+
 }
